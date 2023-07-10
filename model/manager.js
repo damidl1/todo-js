772db890f2e80826704ff5713740c69047118213
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-07-10 12:15:02 +0200

    aggiornamento lezione 2

--- model/manager.js
+++ model/manager.js
@@ -14,4 +14,19 @@ class Manager{
     orderTodosByDate(){
         this.todoArray.sort((todo1, todo2) => todo1.compareByDate(todo2));
     }
+
+
+    deleteTodo(index){
+        this.todoArray.splice(index, 1);
+    }
+
+    
+    addTodoWithTitle(title){
+       const newTodo = new Todo(title);
+       this.addToDo(newTodo);
+    }
 }
+
+
+// creare tasto cancella che richiama la funzione delete to do 
+// creare input di tipo text e tasto aggiungi e nel momento che l'utente clicca aggiungi andrà a controllare se non è una stringa vuota e lo aggiunge alla lista dei todo
